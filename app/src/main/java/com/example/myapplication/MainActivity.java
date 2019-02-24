c28941b8be02e9e3450cc766691728b0c2c63102
Kiiiiiiiiiit <superxanax1@gmail.com>
2019-02-24 09:28:24 +0300

    added now dt + newico

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,6 +13,7 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     TextView temperature;
+    TextView nowDtA;
     Button reload;
 
 
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         temperature = findViewById(R.id.Saratov);
         reload = findViewById(R.id.Reload);
+        nowDtA = findViewById(R.id.NowDt);
         LoadTemperature();
         reload.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,7 +40,29 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(Call<Info> call, Response<Info> response) {
                         int tempA = response.body().getFact().getTemp();
+                        String nowDt = response.body().getNowdt();
+                        System.out.println(nowDt);
+                        StringBuilder temp = new StringBuilder();
+                        temp.append(nowDt.charAt(11));
+                        System.out.println(nowDt.charAt(11));
+
+                        temp.append(nowDt.charAt(12));
+                        System.out.println(nowDt.charAt(12));
+
+                        int saratovTime = Integer.parseInt(temp.toString());
+                        saratovTime +=4;
+                        if(saratovTime>24){
+                            saratovTime=-24;
+                        }
+                        System.out.println(saratovTime);
+                        StringBuilder tempB = new StringBuilder(nowDt);
+                        System.out.println(tempB.toString());
+                        tempB.replace(11,13, String.valueOf(saratovTime));
+                        tempB.replace(10, 11, " ");
+                        tempB.delete(19,24);
+                        System.out.println(tempB.toString());
                         temperature.setText(String.valueOf(tempA));
+                        nowDtA.setText(String.valueOf(tempB.toString()));
                     }
 
                     @Override
